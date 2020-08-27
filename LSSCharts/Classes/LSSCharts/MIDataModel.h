e8194a010a602486789408d3c42879b4d0eb1341
Lushanshan <>
2020-08-27 14:40:02 +0800

    k线图

--- LSSCharts/Classes/LSSCharts/MIDataModel.h
+++ LSSCharts/Classes/LSSCharts/MIDataModel.h
@@ -8,34 +8,33 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "JSONModel.h"
-@interface MIDataModel : JSONModel
-@property (nonatomic,strong) NSString<Optional> * code;//代码
-@property (nonatomic,strong) NSString<Optional> * name;//名称
-@property (nonatomic,strong) NSString<Optional> * isClick;//是否可以点击.
+@interface MIDataModel : NSObject
+@property (nonatomic,strong) NSString * code;//代码
+@property (nonatomic,strong) NSString * name;//名称
+@property (nonatomic,strong) NSString * isClick;//是否可以点击.
 
-@property (nonatomic,strong) NSString<Optional> * zgj;//最高价
-@property (nonatomic,strong) NSString<Optional> * zdj;//最低价
-@property (nonatomic,strong) NSString<Optional> * jksj;//今开市价
-@property (nonatomic,strong) NSString<Optional> * zspj;//昨收盘价
-@property (nonatomic,strong) NSString<Optional> * hqdhl;//行情订货量
-@property (nonatomic,strong) NSString<Optional> * zd;//涨跌
+@property (nonatomic,strong) NSString * zgj;//最高价
+@property (nonatomic,strong) NSString * zdj;//最低价
+@property (nonatomic,strong) NSString * jksj;//今开市价
+@property (nonatomic,strong) NSString * zspj;//昨收盘价
+@property (nonatomic,strong) NSString * hqdhl;//行情订货量
+@property (nonatomic,strong) NSString * zd;//涨跌
 
-@property(nonatomic,strong)  NSString<Optional> *zxj;
-@property (nonatomic,strong) NSString<Optional> * jsj;//结算价
-@property (nonatomic,strong) NSString<Optional> * mj_in;//买价
-@property (nonatomic,strong) NSString<Optional> * mj_out;//卖价
-@property (nonatomic,strong) NSString<Optional> * str1;//占位符1
-@property (nonatomic,strong) NSString<Optional> * str2;//占位符2
-@property (nonatomic,strong) NSString<Optional> * xl;//现量（最新价所对应的成交量）
-@property (nonatomic,strong) NSString<Optional> * bjsj;//报价时间
-@property (nonatomic,strong) NSString<Optional> * isChange;//是否改变
-@property (nonatomic,strong) NSString<Optional> * zdbl;//涨跌比率
-@property (nonatomic,strong) NSString<Optional> *xs;//休市
-@property (nonatomic,strong) NSString<Optional> *syl;//收益率
-@property (nonatomic,strong) NSString<Optional> *cc;//持仓
-@property (nonatomic,strong) NSString<Optional> *ykz;//盈亏
-@property (nonatomic,strong )NSString<Optional> * kmbs;//可买手数
+@property(nonatomic,strong)  NSString *zxj;
+@property (nonatomic,strong) NSString * jsj;//结算价
+@property (nonatomic,strong) NSString * mj_in;//买价
+@property (nonatomic,strong) NSString * mj_out;//卖价
+@property (nonatomic,strong) NSString * str1;//占位符1
+@property (nonatomic,strong) NSString * str2;//占位符2
+@property (nonatomic,strong) NSString * xl;//现量（最新价所对应的成交量）
+@property (nonatomic,strong) NSString * bjsj;//报价时间
+@property (nonatomic,strong) NSString * isChange;//是否改变
+@property (nonatomic,strong) NSString * zdbl;//涨跌比率
+@property (nonatomic,strong) NSString *xs;//休市
+@property (nonatomic,strong) NSString *syl;//收益率
+@property (nonatomic,strong) NSString *cc;//持仓
+@property (nonatomic,strong) NSString *ykz;//盈亏
+@property (nonatomic,strong )NSString * kmbs;//可买手数
 
 
 @end
